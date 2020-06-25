4c7d7fd5c5e183b7208afcb818cfae44c6395227
Max <buxiangzhangda2008@163.com>
2020-06-25 11:10:28 +0800

    贪心算法求数字转罗马数字

---
+++ algo/src/main/java/com/huanglei/algo/Leetcode15.java
@@ -0,0 +1,39 @@
+package com.huanglei.algo;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+ * <p>
+ * 注意：答案中不可以包含重复的三元组。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ * <p>
+ * 满足要求的三元组集合为：
+ * [
+ * [-1, 0, 1],
+ * [-1, -1, 2]
+ * ]
+ */
+
+public class Leetcode15 {
+
+    public static void main(String args[]) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+    }
+}
